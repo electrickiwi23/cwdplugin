dc074d6723c7373e81e453a3c801115c9d127b28
electrickiwi23 <kiwimeep@gmail.com>
2021-03-26 21:49:28 -0700

    made gamestart static and added support for gamemodes and starting the players with lives and mana

--- src/main/java/me/mintnetwork/initialization/GameStart.java
+++ src/main/java/me/mintnetwork/initialization/GameStart.java
@@ -3,27 +3,36 @@ package me.mintnetwork.initialization;
 import me.mintnetwork.Main;
 import me.mintnetwork.repeaters.Mana;
 import me.mintnetwork.repeaters.Ultimate;
+import me.mintnetwork.wizard.Wizard;
+import me.mintnetwork.wizard.WizardInit;
 
 public class GameStart {
 
     private final Main plugin;
 
-    public boolean gameRunning = false;
+    public static boolean gameRunning = false;
+    public static String gameMode = "";
 
     public GameStart(Main plugin) {
         this.plugin = plugin;
     };
     
-    public void start() {
+    public static void start(Main plugin) {
         // Startup mana generation
         Mana.mana(plugin);
         // Startup ult generation
         Ultimate.ult(plugin);
         // Set gamemode and start gamemode
-        //
+        gameMode = "elimination";
         // Set wizard's lives
-        // 
+        for (Wizard w: WizardInit.playersWizards.values()) {
+            w.ElimLives=3;
+            w.Mana = 3;
+        }
         // Set gameRunning to true
         gameRunning = true;
+
+
+
     }
 }
